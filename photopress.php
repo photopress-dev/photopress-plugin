fe6b53d8c9fc3c7483c49234fa36d29b4b883194
padams <peter@peteradams.org>
2020-08-21 13:51:43 -0700

    bumping version.

--- photopress.php
+++ photopress.php
@@ -7,7 +7,7 @@ Description: Making WordPress work for photographers with beautiful image galler
 Author: Peter Adams
 Author URI: http://www.photopressdev.com
 License: GPL v3
-Version: 1.3.3
+Version: 1.3.4
 */
 
 // If this file is called directly, abort.
@@ -77,7 +77,7 @@ class photopress_plugin {
 					array(
 						'package_name'			=> 'core',
 						'package_label'			=> 'PhotoPress',
-						'version'				=> '1.3.3',
+						'version'				=> '1.3.4',
 						'modules'				=> ['base', 'childpages', 'gallery', 'slideshow', 'metadata'],
 						'dependencies'			=> []
 					)
